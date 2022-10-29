48a838c92ada80fdeda00ac6f62f9c7e7dc1339e
Carlos <cadu.vs@hotmail.com>
2022-10-29 17:29:47 -0300

    feat: Abstract Model class

---
+++ src/main/java/com/carona/caronasfesa/models/AbstractModel.java
@@ -0,0 +1,15 @@
+package com.carona.caronasfesa.models;
+
+public class AbstractModel {
+
+    protected Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
