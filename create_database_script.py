e3387da8611ffb5620c50bf757f7db4f13e3b109
Carlos <cadu.vs@hotmail.com>
2022-11-06 17:04:29 -0300

    feat: early database generator script

---
+++ create_database_script.py
@@ -0,0 +1,47 @@
+import sqlite3 as sql
+import pandas as pd
+
+conn = None
+
+try:
+    conn = sql.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.executescript(
+        """
+        DROP TABLE IF EXISTS [User];
+        CREATE TABLE IF NOT EXISTS [User] (
+            [id] INTEGER PRIMARY KEY,
+            [name] TEXT,
+            [description] TEXT,
+            [course] INTEGER,
+            [phone_number] TEXT,
+            [password] TEXT
+        );
+        
+        DROP TABLE IF EXISTS [Location];
+        CREATE TABLE IF NOT EXISTS [Location] (
+            [id] INTEGER PRIMARY KEY,
+            [latitude] REAL,
+            [longitude] REAL
+        );
+        
+        DROP TABLE IF EXISTS [Post];
+        CREATE TABLE IF NOT EXISTS [Post] (
+            [id] INTEGER PRIMARY KEY,
+            [title] TEXT,
+            [description] TEXT,
+            [place_of_departure] INTEGER,
+            [destination] INTEGER,
+            [available_weekdays] TEXT,
+            available_seats INTEGER,
+            departure_time TEXT,
+            FOREIGN KEY([place_of_departure]) REFERENCES [Location](id),
+            FOREIGN KEY([destination]) REFERENCES [Location](id)
+        );
+        """
+    )
+
+finally:
+    if conn:
+        conn.close()
