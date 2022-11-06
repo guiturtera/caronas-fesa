b008eb0d433bfe5b53907cc2c96f48a5feb97ae0
Carlos <cadu.vs@hotmail.com>
2022-11-06 17:39:41 -0300

    fix: DAO methods may throw SQLException

--- src/main/java/com/carona/caronasfesa/DAO/GenericDAO.java
+++ src/main/java/com/carona/caronasfesa/DAO/GenericDAO.java
@@ -1,17 +1,18 @@
 package com.carona.caronasfesa.DAO;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface GenericDAO<E> extends Serializable {
 
-    void insert(E e);
+    void insert(E e) throws SQLException;
 
-    void update(E e);
+    void update(E e) throws SQLException;
 
-    void remove(E e);
+    void remove(E e) throws SQLException;
 
-    E readById(E e);
+    E readById(E e) throws SQLException;
 
-    List<E> readAll();
+    List<E> readAll() throws SQLException;
 }
